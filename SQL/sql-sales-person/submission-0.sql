0dfe71d132efc0c1ec6377c16f0c1fdb72159ae1
Vidya <vidhyasuresh416@gmail.com>
2026-04-10 17:27:41 +0530

    Add: sql-sales-person - submission-0

---
+++ SQL/sql-sales-person/submission-0.sql
@@ -0,0 +1,8 @@
+-- Write your query below
+select name 
+from sales_person
+where sales_id not in(
+    select o.sales_id 
+    from orders o 
+    join company c on o.com_id = c.com_id
+    where c.name = 'CRIMSON');
